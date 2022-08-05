c350ce6702587a34bace12c974a064081607ca5d
Sadikshya <pasishya@gmail.com>
2022-08-05 16:59:25 +0545

    login page added

---
+++ pixel/lib/screens/buyer/b_login.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:pixel/screens/widgets/header_widget.dart';
+import 'package:pixel/screens/widgets/theme_helper.dart';
+
+
+class BLogin extends StatefulWidget {
+  const BLogin({Key? key}) : super(key: key);
+
+  @override
+  State<BLogin> createState() => _BLoginState();
+}
+
+class _BLoginState extends State<BLogin> {
+  double _headerHeight=250;
+  Key _formKey= GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: _headerHeight,
+              child: HeaderWidget(_headerHeight,true, Icons.login_rounded)
+
+            ),
+            SafeArea(
+              child: Container(
+                child : Column(
+                  children: [
+                    Text('LOGIN', style:TextStyle(fontSize: 60,fontWeight: FontWeight.bold,)),
+                    SizedBox(height:30),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: ThemeHelper().textInputDecoration('UserName','Enter your username'),
+                          ),
+                          SizedBox(height:10),
+                          TextField(
+                            obscureText: true,
+                            decoration: ThemeHelper().textInputDecoration('Password','Enter your password'),
+                          ),
+                          SizedBox(height: 40,),
+                          Container(
+                            child: Text('Forgot password?'),
+                          ),
+
+                          SizedBox(height: 20,),
+                          Container(
+                            decoration:ThemeHelper().buttonBoxDecoration(context),
+                            child: ElevatedButton(
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                child: Text('Login'),
+                              ),
+                              onPressed: (){},)
+                              
+                            
+                          ),
+                          SizedBox(height: 20,),
+                          
+
+                          Container(
+                            child: Text('Don\'t have account? Sign Up'),
+                          ),
+
+                          
+                          
+                          
+
+                        ],))
+
+                  ]
+                )
+              ),)])
+          ,));
+    
+  }
+}
---
+++ pixel/lib/screens/widgets/theme_helper.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ThemeHelper{
+
+  InputDecoration textInputDecoration([String lableText="", String hintText = ""]){
+    return InputDecoration(
+      labelText: lableText,
+      hintText: hintText,
+      fillColor: Colors.white,
+      filled: true,
+      contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.grey)),
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.grey.shade400)),
+      errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.red, width: 2.0)),
+      focusedErrorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(100.0), borderSide: BorderSide(color: Colors.red, width: 2.0)),
+    );
+  }
+  BoxDecoration buttonBoxDecoration(BuildContext context) {
+    Color c1 = Theme.of(context).primaryColor;
+    Color c2 = Theme.of(context).primaryColor;
+   
+    return BoxDecoration(
+      boxShadow: [
+        BoxShadow(color: Colors.black26, offset: Offset(0, 4), blurRadius: 10.0)
+      ],
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        stops: [0.0, 1.0],
+        colors: [
+          c1,c2,
+        ],
+      ),
+      color: Color.fromARGB(255, 82, 0, 224),
+      borderRadius: BorderRadius.circular(30),
+    );
+  }
+
+}
